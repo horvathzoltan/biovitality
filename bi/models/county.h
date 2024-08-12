c17669d7cfdf9dd797af07d0d9a14b33b565f9c9
Horváth Zoltán <horvath.zoltan.mobil@gmail.com>
2024-08-12 22:07:52 +0200

    12

--- bi/models/county.h
+++ bi/models/county.h
@@ -1,12 +1,24 @@
 #ifndef COUNTY_H
 #define COUNTY_H
 
+#include "bi/meta/meta.h"
+#include <QDateTime>
+#include <QList>
+#include <QVariant>
+
+#include <helpers/filehelper.h>
+#include <helpers/sqlhelper.h>
+
 class County
 {
 public:
     County();
 
     int id=-1;
+    QString partnerName;
+    QString KSH_code;
+
+    //static void MetaInit();
 };
 
 #endif // COUNTY_H
