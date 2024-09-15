252188a573e26034e1c1468cc247f7f30b7ab100
Horváth Zoltán <horvath.zoltan.mobil@gmail.com>
2024-09-15 20:08:52 +0200

    12

--- bi/models/address.cpp
+++ bi/models/address.cpp
@@ -1,3 +1,25 @@
 #include "address.h"
 
+Meta<Address> Address::_meta;
+
 Address::Address() {}
+
+void Address::MetaInit()
+{
+    AddMetaBase(Address);
+    AddMetaField(id);
+    AddMetaField(postalCode);
+    AddMetaField(settlementName);
+    AddMetaField(publicAreaName);
+    AddMetaField(excelId);
+}
+
+bool Address::isValid()
+{
+    if(id<0) return false;
+    if(settlementName.isEmpty()) return false;
+    if(publicAreaName.isEmpty()) return false;
+    //if(units<=0) return false; // ha minusz a darabszám, stornós
+    //if(netPrice<=0) return false; // ha minusz az ár, stornós
+    return true;
+}
--- bi/models/address.h
+++ bi/models/address.h
@@ -17,7 +17,21 @@ public:
     //QString houseNumber;    // házszám - a cím utolsó tagja
     int excelId=-1;
 
+    bool isValid();
+
+    // meta
+private:
+    static Meta<Address> _meta;
+public:
     static void MetaInit();
+    // sqlrepo
+    static QString GetMetaFieldList(){ return _meta.GetFieldList();}
+    //static QString GetMetaFieldList_UPDATE(){ return _meta.GetFieldList_UPDATE();}
+    static Address FromMetaValues(const QList<MetaValue> &v){return _meta.FromMetaValues(v);}
+    QList<MetaValue> GetMetaValues()const { return _meta.ToMetaValues(this);}
+    QString GetBaseTypeName() {return _meta.GetBaseTypeName();}
+    QList<SQLHelper::SQLParam> GetQueryParams()const { return _meta.ToMetaValues2(this);}
+
 };
 
 #endif // ADDRESS_H
